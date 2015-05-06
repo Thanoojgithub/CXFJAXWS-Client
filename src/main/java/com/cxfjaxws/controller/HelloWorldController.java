cfce429ed903572cd518b5f58f33a94181b02b1d
javaeecub@gmail.com <javaeecub@gmail.com>
2015-05-06 21:06:53 +0530

    response, request

--- src/main/java/com/cxfjaxws/controller/HelloWorldController.java
+++ src/main/java/com/cxfjaxws/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.cxfjaxws.controller;
 
+import java.math.BigInteger;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +12,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.cxfjaxws.EmployeeRequest;
+import com.cxfjaxws.EmployeeResponse;
 import com.cxfjaxws.HelloWorld;
+import com.cxfjaxws.ObjectFactory;
 
 @Controller
 public class HelloWorldController {
@@ -22,14 +27,17 @@ public class HelloWorldController {
 	private HelloWorld helloWorldClient;
 
 	@RequestMapping(value = "/sayHi", method = RequestMethod.GET)
-	public @ResponseBody String sayHi(@RequestParam("name") String name) {
-		logger.info("sayHi starts");
-		String sayHi = helloWorldClient.sayHi(name);
+	public @ResponseBody EmployeeResponse getEmployee(@RequestParam("id") BigInteger id) {
+		logger.info("getEmployee starts");
+		ObjectFactory objectFactory = new ObjectFactory();
+		EmployeeRequest employeeRequest = objectFactory.createEmployeeRequest();
+		employeeRequest.setId(id);
+		EmployeeResponse employee = helloWorldClient.getEmployee(employeeRequest);
 		logger.info("##################");
-		logger.info(sayHi);
+		logger.info("employee :: "+employee);
 		logger.info("##################");
-		logger.info("sayHi ends");
-		return sayHi;
+		logger.info("getEmployee ends");
+		return employee;
 	}
 
 }
